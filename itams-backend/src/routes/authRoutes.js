135c37f107199e55c8b8d4b3821c2867daf247c5
meghana110506 <24211a6701@bvrit.ac.in>
2026-07-03 19:07:11 +0530

    fixed schema and otp mails

--- itams-backend/src/routes/authRoutes.js
+++ itams-backend/src/routes/authRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, sendOtp, verifyOtp, resetPassword } = require('../controllers/authController');
+const { login, sendOtp, verifyOtp, resetPassword } = require('../controllers/authController');
 
-router.post('/register', register);
 router.post('/login', login);
 router.post('/forgot-password/send-otp', sendOtp);
 router.post('/forgot-password/verify-otp', verifyOtp);
